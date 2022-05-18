b350110853bebb222efb421dedd0e00464035247
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-18 16:24:38 +0700

    Update iklanbot.js

--- plugins/iklanbot.js
+++ plugins/iklanbot.js
@@ -14,9 +14,9 @@ let handler = async(m, { conn }) => {
         {title: 'Jasa run mursid', description: "jasa run bot", rowId:".runbotmursid"},
         {title: 'Pasang iklan di bot', description: "jasa pemasang iklan di bot", rowId:".lucbot"},
         {title: 'Jasa tutorial run replit', description: "tutorial replit", rowId:".replit2"},
-        {title: 'Jasa tutorial run heroku', description: "tutorial heroku", rowId:".horoku3"},
+        {title: 'Jasa tutorial run heroku', description: "tutorial heroku", rowId:".heroku3"},
         {title: 'KEMBALI KE HALAMAN UTAMA', description: "menampilkan menu", rowId:".menu"},
-        {title: 'RULES', description: "taati peraturan", rowId:".rules"},
+        {title: 'RULES', description: "taati peraturan", rowId:".rules2"},
        ] }],
         listType: 1
        }
