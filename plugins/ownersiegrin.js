aec434a8447fe137f1f3631b4767e9800a0c88db
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-18 13:23:13 +0700

    Create ownersiegrin.js

---
+++ plugins/ownersiegrin.js
@@ -0,0 +1,19 @@
+const { MessageType } = require('@adiwajshing/baileys')
+const PhoneNumber = require('awesome-phonenumber')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  await conn.sendMessage(m.chat, {
+    contacts: [{
+    "displayName": "SIEGRIN",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:SIEGRIN;;;\nFN:SIEGRIN\nitem1.TEL;waid=6289530298948:6289530298948\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:-\nitem2.X-ABLabel:💌 Email\nitem3.URL:-/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ SIEGRIN ]───────\nEND:VCARD"
+  }, {
+    "displayName": "Whatsapp",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:WHASTAPP;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
+    }]
+  }, MessageType.contactsArray, { quoted: m })
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['ownersiegrin']
+
+handler.command = /^(ownersiegrin)$/i
+
+module.exports = handler
