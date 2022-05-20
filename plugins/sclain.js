55b74f8d59b6ae4ce9459f1607142805eb7ea037
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-20 11:58:33 +0700

    Update sclain.js

--- plugins/sclain.js
+++ plugins/sclain.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   let ext= `
   Status : *_sc yg bot gunakan_* 
 *📮: SC NO ENC! Harap tidak dijual belikan*
-https://github.com/Botynyakamu/zifabotz-V11
+https://github.com/Botynyakamu/Rzy.botz-M1
 `.trim()
 conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'SCRIPT')).buffer(), ext, '©zifabotz', 'MENU', '.menu', 'OWNER', '.owner2', m)
 
