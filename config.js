cbe859a90d54ec62f00dbc344603731c5f068964
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-20 10:34:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,8 @@ global.author = 'zifa'
 global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
 global.eror = '「❗」Server LAGI ERROR COK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.watermark = '© zifabotz' //change the watermark 
+global.wm = ©️zifabotz
+global.watermark = '©️ zifabotz' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
 
