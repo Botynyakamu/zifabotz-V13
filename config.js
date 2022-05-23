89be12bc473d7ccdc066322309b98503b84f189d
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-23 21:39:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // Zifabotz
 
 let fs = require('fs')
-global.owner = ['6285828764046', '6285828764046','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
+global.owner = ['6285828530078', '6285828530078','6285828764046', '6285828764046', '6285828764046'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -43,12 +43,12 @@ global.APIKeys = { // APIKey nya disini
 // Sticker WM
 global.packname = 'rozi'
 global.author = 'zifa'
+global.wm = '©zifabotz'
 
 global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
 global.eror = '「❗」Server LAGI ERROR COK!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
-global.wm = ©️zifabotz
-global.watermark = '©️ zifabotz' //change the watermark 
+global.watermark = '© zifabotz' //change the watermark 
 global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
 global.thumbfoto = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
 
