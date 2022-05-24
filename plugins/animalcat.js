debb09706e9c49d022f7b0eab1a0356e5e3cd8fb
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-24 10:21:24 +0700

    Update animalcat.js

--- plugins/animalcat.js
+++ plugins/animalcat.js
@@ -23,7 +23,7 @@ contoh:
   );
   if (!res.ok) throw `${res.status} ${res.statusText}`;
   let json = await res.json();
-  if (json.image) await conn.sendFile(m.chat, json.image, "", `${json.fact}\n\n~fatur`, m);
+  if (json.image) await conn.sendFile(m.chat, json.image, "", `${json.fact}\n\n©zifabotz`, m);
   else throw json;
 };
 handler.help = ["animal"].map((v) => v + " <hewan>");
