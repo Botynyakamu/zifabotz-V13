a338f8350ed842058acc26f70e052f2139b2b2d6
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-25 01:20:03 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -2,7 +2,7 @@
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '')
   for (let id of chats) conn.copyNForward(id, content)
-  conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
+  conn.reply(m.chat, `_Berhasil mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }*/
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, text }) => {
@@ -10,7 +10,8 @@ let handler  = async (m, { conn, text }) => {
   let thumb = 'https://telegra.ph/file/725e210db4f901bdc9eb9.jpg'
   let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
   let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text )
-  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n_*ALL BROADCAST*_\n${time}`, 'Owner', '.owner2', 'Menu', '.menu', /*'Donasi', '.ds'*/)
+  for (let id of chats) /*conn.send2ButtonLoc*/conn.send2Button(id, `${text}`.trim(), `\n╭─[ *_ZIFABOTZ_* ]─✧
+╰─────···\n${time}`, 'Owner', '.owner2', 'Menu', '.menu', /*'Donasi', '.ds'*/)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
 }
 handler.help = ['broadcast', 'bcbutton', 'bc'].map(v => v + ' <teks>')
