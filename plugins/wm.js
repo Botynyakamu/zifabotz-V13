f6e2ba567c3b31d073a27493c99d3ba34f801f12
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-30 16:00:31 +0700

    Update wm.js

--- plugins/wm.js
+++ plugins/wm.js
@@ -1,43 +1,26 @@
-const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
-const uploadFile = require('../lib/uploadFile')
-const uploadImage = require('../lib/uploadImage')
-let { webp2png } = require('../lib/webp2mp4')
-let handler = async (m, { conn, text }) => {
+let { addExif } = require('../lib/sticker.js')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!m.quoted) throw `Balas stiker dengan perintah *${usedPrefix + command}*`
   let stiker = false
   try {
-    let [packname, ...author] = text.split`|`
-    author = (author || []).join`|`
-    let q = m.quoted ? m.quoted : m
+    let [packname, ...author] = text.split('|')
+    author = (author || []).join('|')
     let mime = m.quoted.mimetype || ''
-    if (/webp/.test(mime)) {
-      let img = await q.download()
-      let out = await webp2png(img)
-      if (!img) throw `balas stiker dengan perintah ${usedPrefix + command} <packname>|<author>`
-      stiker = await sticker(0, out, packname || '', author || '')
-    } else if (/image/.test(mime)) {
-      let img = await q.download()
-      let link = await uploadImage(img)
-      if (!img) throw `balas gambar dengan perintah ${usedPrefix + command} <packname>|<author>`
-      stiker = await sticker(0, link, packname || '', author || '')
-    } else if (/video/.test(mime)) {
-      if ((q.msg || q).seconds > 11) throw 'Maksimal 10 detik!'
-      let img = await q.download()
-      let link = await uploadFile(img)
-      if (!img) throw `balas video dengan perintah ${usedPrefix + command} <packname>|<author>`
-      stiker = await sticker(0, link, packname || '', author || '')
-    }
+    if (!/webp/.test(mime)) throw 'Balas stikernya cokk!'
+    let img = await m.quoted.download()
+    if (!img) throw 'Balas Stiker!'
+    stiker = await addExif(img, packname || '', author || '')
+  } catch (e) {
+    console.error(e)
+    if (Buffer.isBuffer(e)) stiker = e
   } finally {
-    if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
-      quoted: m
-    })
-    else throw 'Balas stikernya!'
+    if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+    else throw 'Gagal ngasih wm cokk!\nusahakan kirim terus balas stikernya ya cokk!'
   }
 }
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^wm$/i
-
-handler.limit = true
 
 module.exports = handler
