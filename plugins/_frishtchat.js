1edb52ec1cb50ca4c6a5e5f0af9dddc46b328284
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-01 01:49:08 +0700

    Update _frishtchat.js

--- plugins/_frishtchat.js
+++ plugins/_frishtchat.js
@@ -1,46 +1,17 @@
-// RECODE ALYAAXZY
-
-let moment = require('moment-timezone')
-let fetch = require ('node-fetch')
 let handler = m => m
 
-handler.all = async function (m) {
-
-    if (m.chat.endsWith('broadcast')) return
-    if (m.fromMe) return
-    if (m.isGroup) return
-    if (db.data.settings.groupOnly) return
-    let user = global.db.data.users[m.sender]
-    let name = conn.getName(m.sender)
-    if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-    await conn.send2ButtonLoc(m.chat, await(await fetch(thumbfoto)).buffer(), `
-*Hi ${name}, ${ucapan()}*
-Saya adalah zifabotz, Bot salah satu bot WhatsApp.
-
-*Harap untuk tidak menelpon, meminta save, atau spam dalam penggunaan bot*
-Silahkan baca rules dan mematuhi rules demi kenyamanan kita bersama.
-Terimakasih
-
-Jika ada kendala silahkan hubungi owner.
-`.trim(), watermark, 'Menu', '.?', 'Rules', '.rules2')
-    user.pc = new Date * 1
+handler.all = async function (m, { isBotAdmin }) {
+    // auto clear ketika terdapat pesan yang tidak dapat dilihat di wa desktop
+    if (m.messageStubType === 68) {
+        let log = {
+            key: m.key,
+            content: m.msg,
+            sender: m.sender
+        }
+        await this.modifyChat(m.chat, 'clear', {
+            includeStarred: false
+        }).catch(console.log)
+    }
 }
 
 module.exports = handler
-function ucapan() {
-    const time = moment.tz('Asia/Jakarta').format('HH')
-    res = "Selamat dinihari"
-    if (time >= 4) { 
-        res = "Selamat pagi"
-    }
-    if (time > 10) {
-        res = "Selamat siang"
-    }
-    if (time >= 15) {
-        res = "Selamat sore"
-    }
-    if (time >= 18) {
-        res = "Selamat malam"
-    }
-    return res
-}
