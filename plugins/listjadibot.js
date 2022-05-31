678fe85e1c3b3fe3b802d9d39767a412662fa443
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 10:26:53 +0700

    Update listjadibot.js

--- plugins/listjadibot.js
+++ plugins/listjadibot.js
@@ -1,8 +1,9 @@
 async function handler(m, { usedPrefix }) {
   let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(`🧑‍💻 *Owner ZIFABOTZ*\n\nwa.me/6287773973818\n\n🌟 *Bot Utama* \n\n• https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🪢JadiBot* \n\n` + users.map(v => '• https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
+  m.reply(`💌 *Owner Botz*\n\nhttps://wa.me/6287773973818?text=Hallo+owner+zifabotz\n\n🏅 *Bot Utama* \n\n➺ https://wa.me/${global.conn.user.jid.split('@')[0]}?text=${usedPrefix}menu (${global.conn.user.name}) \n\n *🍃 JadiBot* \n\n` + users.map(v => '❥ https://wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${usedPrefix}menu (${v.name})`).join('\n'))
 }
-handler.command = handler.help = ['listjadibot','ljb']
+handler.command = handler.help = ['listjadibot', 'listbot', 'ljb']
 handler.tags = ['jadibot']
 
 module.exports = handler
+// Ubah
