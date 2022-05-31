1e8229c82c6c8909eb723983d7399c618d0c6dce
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 19:41:14 +0700

    Update broadcasloc.js

--- plugins/broadcasloc.js
+++ plugins/broadcasloc.js
@@ -1,12 +1,12 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-    let logo = global.logo
+    let image = global.image
     let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
     conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
     for (let id of chats) {
         await delay(1500)
-        await conn.send2ButtonLoc(id, logo,'*—「 Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
+        await conn.send2ButtonLoc(id, image,'*—「 Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
     }
     m.reply('_*Broadcast Finished*_')
 }
