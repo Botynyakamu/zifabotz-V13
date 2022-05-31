b492694ccb33040ed4f9aafb149853351239c7a5
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 19:37:29 +0700

    Update _frishtchat.js

--- plugins/_frishtchat.js
+++ plugins/_frishtchat.js
@@ -9,9 +9,9 @@ let username = conn.getName(who)
 let logo = await (await fetch(`${logos()}`)).buffer()
 let tag = `@${m.sender.split('@')[0]}`
  m, { contextInfo: { mentionedJid: conn.parseMention(tag) }}
-    let waofc = `@${'0'.split('@')[0]}`
+    let pembantu = `@${'6285828764046'.split('@')[0]}`
  m, { contextInfo: { mentionedJid: conn.parseMention(tag) }}
- let ow = `@${'6285828764046'.split('@')[0]}`
+ let ow = `@${'6287773973818'.split('@')[0]}`
  m, { contextInfo: { mentionedJid: conn.parseMention(tag) }}
 
     if (m.chat.endsWith('broadcast')) return
@@ -22,7 +22,7 @@ let tag = `@${m.sender.split('@')[0]}`
     let tks = `Saya adalah ZIFABOTZ salah satu Bot Whatsapp yang diciptakan oleh : ${ow}
 Silahkan tekan Tombol ${user.registered ? '"Menu"' : '"Menu"'} untuk ${user.registered ? 'melihat list command dari' : 'list menu di zifabotz'} zifabotz.
 
-*Official Bot By ${waofc}*
+*Official Bot By ${pembantu}*
 *Powered By ${ow}*`
     const ftroli = {
     key : {
