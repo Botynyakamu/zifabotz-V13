ae5b4c493804303f532e46732e47110090a2a298
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 15:12:02 +0700

    Update banchat.js

--- plugins/banchat.js
+++ plugins/banchat.js
@@ -1,24 +1,32 @@
 let handler = async (m, { conn, isOwner, text, isAdmin }) => {
-  let who
+//y = groupMetadata
+//m.reply(y.subject)
+ let who
   if (m.isGroup) {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn)
+      throw false
+    }
     if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
     else who = m.chat
   } else {
-    if (!isOwner) return dfail('owner', m, conn)
+    if (!isOwner) {
+      global.dfail('owner', m, conn)
+      throw false
+    }
     who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
   }
 
   try {
-    if (who.endsWith('g.us')) db.data.chats[who].isBanned = true
-    else db.data.users[who].banned = true
-    m.reply(`*${conn.user.name} sekarang tidak aktif dichat ${conn.getName(who) == undefined ? 'ini' : conn.getName(who)}.`)
+    if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = true
+    else global.db.data.users[who].banned = true
+    m.reply(`${namabot} tidak aktif dichat @${who.split`@`[0]}`) //== undefined ? 'ini' : (y.subject)`)
   } catch (e) {
-    throw `jid tidak ada didatabase!`
+    throw `nomor tidak ada didatabase!`
   }
 }
 handler.help = ['ban']
-handler.tags = ['owner']
+handler.tags = ['owner', 'group']
 handler.command = /^ban(chat)?$/i
-
+handler.mods = true
 module.exports = handler
