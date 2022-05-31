1ef63dcefcbf1d09eac4d13882b3ccccef56e787
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-05-31 20:01:37 +0700

    Update broadcasloc.js

--- plugins/broadcasloc.js
+++ plugins/broadcasloc.js
@@ -1,18 +1,18 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-    let logo = global.logo
+    let logo = global.bcloc
     let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
     conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
     for (let id of chats) {
         await delay(1500)
-        await conn.send2ButtonLoc(id, logo,'*—「 Broadcast 」—*\n' + text, wm, '⋮☰ Menu', '.menu2', 'Donasi', '.donasi')
+        await conn.send2ButtonLoc(id, logo,'*—「 Broadcast Zifabotz 」—*\n' + text, wm, '⋮☰ Menu', '.menu', 'Donasi', '.donasi')
     }
     m.reply('_*Broadcast Finished*_')
 }
-handler.help = ['bc'].map(v => v + ' <teks>')
+handler.help = ['broadcastloc'].map(v => v + ' <teks>')
 handler.tags = ['owner']
-handler.command = /^(broadcast|bc)$/i
+handler.command = /^(broadcastloc|bcloc)$/i
 
 handler.owner = true
 
